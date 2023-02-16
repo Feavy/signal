e03096010d885414bd939cbb241694af76f82f01
Feavy <quentin071100@live.fr>
2023-02-16 15:29:24 +0100

    feat: batching

---
+++ src/reactive/batch.ts
@@ -0,0 +1,34 @@
+import Observer from "./Observer";
+
+export module Batch {
+  const observers: Set<Observer> = new Set();
+  let _isBatching = false;
+
+  export function add(observer: Observer) {
+    observers.add(observer);
+  }
+
+  export function addAll(observers: Observer[]) {
+    observers.forEach(observer => add(observer));
+  }
+
+  export function start() {
+    _isBatching = true;
+  }
+
+  export function end() {
+    _isBatching = false;
+    observers.forEach(observer => observer.trigger());
+    observers.clear();
+  }
+
+  export function isBatching() {
+    return _isBatching;
+  }
+}
+
+export function batch(callback: () => void) {
+  Batch.start();
+  callback();
+  Batch.end();
+}
